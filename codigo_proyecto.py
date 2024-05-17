180617884f5927c94af846017234b49052e08584
Gabriel Rendón <gjrendon@correo.url.edu.gt>
2024-05-17 09:12:28 -0600

    nuevos cambios

--- codigo_proyecto.py
+++ codigo_proyecto.py
@@ -14,13 +14,35 @@ numeros_aleatoriosest1 = []
 numeros_aleatoriosest2 = []
 numeros_aleatorioest3 = []
     
+def ingresar_supervisor ():
+    print ("Ingresar supervisor")
+    print ("Ingrese el código de identificación")
+    codigoid.append (input())
+    estación.append ("Supervisor")
+    print ("Ingrese el nombre del supervisor")
+    nombre.append (input())
+    print ("Ingrese el apellido del supervisor")
+    apellido.append (input())
+    print ("Ingrese el día de nacimiento")
+    diadenacimiento.append (input())
+    print ("Ingrese el mes de nacimiento")
+    mesdenacimiento.append (input())
+    print ("Ingrese el año de nacimiento")
+    añodenacimiento.append (input())
+    print ("Ingrese la contraseña")
+    contraseña.append (input())
+    print ("Supervisor registrado con éxito")
 
 def ingresar_operarios ():
     print ("Ingresar operarios")
     print ("Ingrese el codigo de identificación")
     codigoid.append (input())
     print ("Ingrese la estación de trabajo")
-    estación.append (input())
+    estacn = input ()
+    if estación >= 1 and estación <= 3:
+        estación.append (estacn)
+    else:
+        print ("Estación no válida")        
     print ("Ingrese el nombre del operario")
     nombre.append (input())
     print ("Ingrese el apellido del operario")
@@ -51,16 +73,19 @@ def simular_paso_del_tiempo():
     print("Simular paso del tiempo")
     for i in range(5):
         numeroest1 = randint(75, 120)
+        numeroest1 = int(numeroest1 - numeroest1 * 0.1)      
         numeros_aleatoriosest1.append(numeroest1)
     for i in range(5):
         numeroest2 = randint(75, 120)
+        numeroest2 = int(numeroest2 - numeroest2 * 0.06)
         numeros_aleatoriosest2.append(numeroest2)
     for i in range(5):
         numeroest3 = randint(75, 120)
+        numeroest3 = int(numeroest3 - numeroest3 * 0.03)
         numeros_aleatorioest3.append(numeroest3)
 
 def historial_de_producción ():
-    i = 1
+    i = 0
     print ("Historial de producción")
     print ("Estación 1")
     print ("Producto ", numeros_aleatoriosest1[i], numeros_aleatoriosest1[i+1], numeros_aleatoriosest1[i+2], numeros_aleatoriosest1[i+3], numeros_aleatoriosest1[i+4])
@@ -152,7 +177,7 @@ def cambiar_contraseña ():
     else:
         print ("Contraseña incorrecta")
         
-
+ingresar_operarios()
 while True:
     print ("Bienvenido al sistema de registro de usuarios")
     print ("Seleccione una opción")
@@ -172,27 +197,56 @@ while True:
         os.system('cls' if os.name == 'nt' else 'clear')
     elif opcion == "2":
         mostrar_equipo_de_trabajo()
+        print ("Presione enter para continuar")
+        input()
+        os.system('cls' if os.name == 'nt' else 'clear')     
     elif opcion == "3":
         simulación = "s"
-        simular_paso_del_tiempo()    
+        simular_paso_del_tiempo()  
+        print ("Presione enter para continuar")
+        input()
+        os.system('cls' if os.name == 'nt' else 'clear') 
     elif opcion == "4":
         if simulación == "s":
             historial_de_producción()
+            print ("Presione enter para continuar")
+            input()
+            os.system('cls' if os.name == 'nt' else 'clear')
         else:
             print ("Favor de realizar la simulación del paso del tiempo")
+            print ("Presione enter para continuar")
+            input()
+            os.system('cls' if os.name == 'nt' else 'clear')
     elif opcion == "5":
         if simulación == "s":
             control_de_calidad()
+            os.system('cls' if os.name == 'nt' else 'clear')    
         else:
             print ("Favor de realizar la simulación del paso del tiempo")
+            print ("Presione enter para continuar")
+            input()
+            os.system('cls' if os.name == 'nt' else 'clear')
     elif opcion == "6":
         if simulación == "s":
             pago_a_operarios()
+            print ("Presione enter para continuar")
+            input()
+            os.system('cls' if os.name == 'nt' else 'clear')
         else:
             print ("Favor de realizar la simulación del paso del tiempo")
+            print ("Presione enter para continuar")
+            input()
+            os.system('cls' if os.name == 'nt' else 'clear')
     elif opcion == "7":
         reestablecer_contraseña()
+        print ("Presione enter para continuar")
+        input()
+        os.system('cls' if os.name == 'nt' else 'clear')
     elif opcion == "8":
         cambiar_contraseña()
+        print ("Presione enter para continuar")
+        input()
+        os.system('cls' if os.name == 'nt' else 'clear')
     elif opcion == "9":
+        print ("Gracias por utilizar el sistema")
         break
